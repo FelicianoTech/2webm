ceb0b78ae3a2b51e217f4fc8e719a145227f5e7c
Lev Lazinskiy <lev@levlaz.org>
2015-05-20 23:53:05 -0700

    Removed Old Python Files

--- config.py
+++
@@ -1,9 +0,0 @@
-import os 
-_basedir = os.path.absolute(os.path.dirname(__file__))
-
-config = open("config/settings.yaml", "r") 
-settings = yaml.load(config) 
-
-connection = "mysql://%s:%s@%s:3306/%s" % (
-	settings['username'], settings['password'], 
-	settings['hostname'], settings['database'])
